b2c1a14e799636968401d5d93c8742f2d9ce4fa5
Juan Sebastian Sotomayor <juanse.sotomayor77@gmail.com>
2023-05-19 14:31:37 -0500

    bases

---
+++ Angular/01-typescript-intro/src/main.ts
@@ -0,0 +1,8 @@
+import './style.css'
+import './topics/04-homework-types'
+
+document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
+  hola mundo
+`
+
+
---
+++ Angular/01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,7 @@
+const name = 'Juancho';
+
+let hpPoints: number | 'FULL' = 95;
+
+hpPoints = 'FULL';
+
+export {};
